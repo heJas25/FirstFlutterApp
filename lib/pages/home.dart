f5d5d074dd5bdef9dc8a44c93e9a47f2f8c3ed96
Yasmine <yasmineharfouche0@gmail.com>
2025-09-16 20:04:36 +0200

    commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'desserts',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0 ,
+      ),
+    );
+  }
+}
