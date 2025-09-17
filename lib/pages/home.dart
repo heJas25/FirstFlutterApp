dc1f040cce745ba85dd76d5825ff3d18706bb71c
Yasmine <yasmineharfouche0@gmail.com>
2025-09-17 14:23:23 +0200

    commit

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,44 +1,74 @@
 import 'package:flutter/material.dart';
 
-
 class MyHome extends StatelessWidget {
   const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'desserts',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff1D1617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0,
+                ),
+              ],
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
           ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Color.fromARGB(255, 245, 215, 250),
-          ),
-          child: const Icon(Icons.arrow_back_ios_rounded ,weight: 17),
-        ),
-        actions: 
-        [ Container(
-          alignment: Alignment.center,
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: Color.fromARGB(255, 245, 215, 250),
-          ),
-          child: const Icon(Icons.plus_one_rounded ,weight: 17),
-        ),],
+        ],
       ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'desserts',
+        style: TextStyle(
+          color: const Color.fromARGB(255, 29, 17, 25),
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: Container(
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color.fromARGB(255, 245, 220, 249),
+        ),
+        child: const Icon(Icons.arrow_back_ios_rounded, weight: 17),
+      ),
+      actions: [
+        Container(
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color.fromARGB(255, 245, 215, 250),
+          ),
+          child: const Icon(Icons.more_vert, weight: 17),
+        ),
+      ],
     );
   }
 }
